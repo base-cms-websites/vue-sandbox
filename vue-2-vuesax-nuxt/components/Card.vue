99e7950b7b61a8d755e66118c469c68863033a7b
Alysha Buselt <Alixers505@users.noreply.github.com>
2020-10-12 10:07:40 -0400

    Add card component

---
+++ vue-2-vuesax-nuxt/components/Card.vue
@@ -0,0 +1,16 @@
+<template class="ebm-component--card">
+  <vs-card>
+    <div slot="title">
+      <h3>Working from Home</h3>
+    </div>
+    <div slot="img">
+      <img
+        src="https://images.unsplash.com/photo-1593642532744-d377ab507dc8?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
+        alt="Plant and Laptop"
+      />
+    </div>
+    <div slot="text">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    </div>
+  </vs-card>
+</template>
